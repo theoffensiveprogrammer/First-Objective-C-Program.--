3ed615c30907b77e0c2c2b1c87e5ef95ec69174d
Keshav Kumar <running992@gmail.com>
2014-03-04 21:45:15 -0800

    Programming is Fun. :)

---
+++ main.m
@@ -0,0 +1,17 @@
+
+// First program example
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Programming is fun!");
+        
+    }
+    return 0;
+}
+
